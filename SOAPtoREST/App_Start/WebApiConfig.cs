8b8c4737cfe2264ebf778bbb2e92b53d71f93c7c
Venkat Iyer <t-viyer@microsoft.com>
2014-07-03 17:32:49 -0700

    Third Commit?
    Testing a third commit.

---
+++ SOAPtoREST/App_Start/WebApiConfig.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Hosting;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace SOAPtoREST
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            
+            JsonSerializer serializer = new JsonSerializer();
+            var file = HostingEnvironment.MapPath("~/map.json");
+            JObject o = (JObject)serializer.Deserialize(new JsonTextReader(new StreamReader(file)));
+            JArray a = (JArray)o.GetValue("mappings");
+
+            // Web API routes
+            /*config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }*/
+
+            config.Routes.MapHttpRoute(
+              name: "Default",
+              routeTemplate: "api/{controller}/{op}",
+              defaults: new
+              {
+                  controller = "SOAPREST",
+                  action = "Handler",
+              },
+              constraints: new
+              {
+                  httpMethod = new HttpMethodConstraint(HttpMethod.Post)
+              }
+
+            );
+        }
+    }
+}
---
+++ SOAPtoREST/Controllers/SOAPRESTController.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Hosting;
+using System.Web.Http;
+
+namespace SOAPtoREST.Controllers
+{
+    public class SOAPRESTController : ApiController
+    {
+        [HttpDelete, HttpHead, HttpOptions, HttpPatch, HttpPut, HttpPost, HttpGet]
+        public HttpResponseMessage Handler(HttpRequestMessage request)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            var file = HostingEnvironment.MapPath("~/map.json");
+            JObject o = (JObject)serializer.Deserialize(new JsonTextReader(new StreamReader(file)));
+            JArray a = (JArray)o.GetValue("mappings");
+
+            var routeData = request.GetRouteData();
+
+            var exampleBody = "";
+            /*foreach (var ex in a)
+            {
+                if (routeData == o.GetValue(){
+                    exampleBody
+                }
+            }*/
+
+            foreach(var kvp in routeData.Values)
+            {
+                exampleBody = exampleBody.Replace("{" + kvp.Key + "}", kvp.Value as string);
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(exampleBody);
+
+            return response;
+        }
+    }
+}
